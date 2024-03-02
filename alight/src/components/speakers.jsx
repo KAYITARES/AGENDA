53cfa891c4cbe97c95df6dc6db18a504f0975822
Franklin-pro <Franklin-pro>
2024-03-02 13:22:34 +0200

    continued

--- alight/src/components/speakers.jsx
+++ alight/src/components/speakers.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+// import { Carousel } from "antd";
 
 function Speakers(){
 
@@ -15,17 +15,41 @@ return (
         <h3>Stay tuned for more Speakers to come</h3>
         </div>
        
+
+     
             <div className="articles">
+           
                 <div className="image">
                     <img src="https://www.paulkagame.com/wp-content/uploads/2021/04/PaulKagame_Web.jpeg" alt="pk"/>
+                    <div className="text">
+                    <h2>H.E POUL KAGAME</h2>
+                    <p>President Republic Of Rwanda chairman of FPR</p>
+                    </div>
+                  
                 
                 <div className="name">
                     <h3>H.E POUL KAGAME</h3>
                 </div>
                 </div>
+                <div className="image">
+                    <img src="https://www.paulkagame.com/wp-content/uploads/2021/04/PaulKagame_Web.jpeg" alt="pk"/>
+                    <div className="text">
+                    <h2>H.E POUL KAGAME</h2>
+                    <p>President Republic Of Rwanda chairman of FPR</p>
+                    </div>
+                
+                <div className="name">
+                    <h3>H.E POUL KAGAME</h3>
+                </div>
+                </div>
+               
 
                 <div className="image">
                     <img src="https://www.paulkagame.com/wp-content/uploads/2021/04/PaulKagame_Web.jpeg" alt="pk"/>
+                    <div className="text">
+                    <h2>H.E POUL KAGAME</h2>
+                    <p>President Republic Of Rwanda chairman of FPR</p>
+                    </div>
                 
                 <div className="name">
                     <h3>H.E POUL KAGAME</h3>
@@ -33,6 +57,10 @@ return (
                 </div>
                 <div className="image">
                     <img src="https://www.paulkagame.com/wp-content/uploads/2021/04/PaulKagame_Web.jpeg" alt="pk"/>
+                    <div className="text">
+                    <h2>H.E POUL KAGAME</h2>
+                    <p>President Republic Of Rwanda chairman of FPR</p>
+                    </div>
                 
                 <div className="name">
                     <h3>H.E POUL KAGAME</h3>
@@ -40,6 +68,10 @@ return (
                 </div>
                 <div className="image">
                     <img src="https://www.paulkagame.com/wp-content/uploads/2021/04/PaulKagame_Web.jpeg" alt="pk"/>
+                    <div className="text">
+                    <h2>H.E POUL KAGAME</h2>
+                    <p>President Republic Of Rwanda chairman of FPR</p>
+                    </div>
                 
                 <div className="name">
                     <h3>H.E POUL KAGAME</h3>
@@ -47,34 +79,22 @@ return (
                 </div>
                 <div className="image">
                     <img src="https://www.paulkagame.com/wp-content/uploads/2021/04/PaulKagame_Web.jpeg" alt="pk"/>
+                    <div className="text">
+                    <h2>H.E POUL KAGAME</h2>
+                    <p>President Republic Of Rwanda chairman of FPR</p>
+                    </div>
                 
                 <div className="name">
                     <h3>H.E POUL KAGAME</h3>
                 </div>
                 </div>
-                <div className="image">
-                    <img src="https://www.paulkagame.com/wp-content/uploads/2021/04/PaulKagame_Web.jpeg" alt="pk"/>
                 
-                <div className="name">
-                    <h3>H.E POUL KAGAME</h3>
-                </div>
-                </div>
-                <div className="image">
-                    <img src="https://www.paulkagame.com/wp-content/uploads/2021/04/PaulKagame_Web.jpeg" alt="pk"/>
+             
+               
                 
-                <div className="name">
-                    <h3>H.E POUL KAGAME</h3>
-                </div>
-                </div>
-                <div className="image">
-                    <img src="https://www.paulkagame.com/wp-content/uploads/2021/04/PaulKagame_Web.jpeg" alt="pk"/>
-                
-                <div className="name">
-                    <h3>H.E POUL KAGAME</h3>
-                </div>
-                </div>
                 
             </div>
+            
             
         </article>
     </>
