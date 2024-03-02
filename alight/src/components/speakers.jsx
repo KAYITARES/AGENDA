1500355b007b6e50e24053bd67ba306ee9ac3b2c
Franklin-pro <Franklin-pro>
2024-03-02 12:25:03 +0200

    our speaker

---
+++ alight/src/components/speakers.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+
+
+function Speakers(){
+
+
+return (
+    <>
+        <article>
+      
+
+        
+        <div className="heads">
+        <h1>OUR SPEAKERS</h1>
+        <h3>Stay tuned for more Speakers to come</h3>
+        </div>
+       
+            <div className="articles">
+                <div className="image">
+                    <img src="https://www.paulkagame.com/wp-content/uploads/2021/04/PaulKagame_Web.jpeg" alt="pk"/>
+                
+                <div className="name">
+                    <h3>H.E POUL KAGAME</h3>
+                </div>
+                </div>
+
+                <div className="image">
+                    <img src="https://www.paulkagame.com/wp-content/uploads/2021/04/PaulKagame_Web.jpeg" alt="pk"/>
+                
+                <div className="name">
+                    <h3>H.E POUL KAGAME</h3>
+                </div>
+                </div>
+                <div className="image">
+                    <img src="https://www.paulkagame.com/wp-content/uploads/2021/04/PaulKagame_Web.jpeg" alt="pk"/>
+                
+                <div className="name">
+                    <h3>H.E POUL KAGAME</h3>
+                </div>
+                </div>
+                <div className="image">
+                    <img src="https://www.paulkagame.com/wp-content/uploads/2021/04/PaulKagame_Web.jpeg" alt="pk"/>
+                
+                <div className="name">
+                    <h3>H.E POUL KAGAME</h3>
+                </div>
+                </div>
+                <div className="image">
+                    <img src="https://www.paulkagame.com/wp-content/uploads/2021/04/PaulKagame_Web.jpeg" alt="pk"/>
+                
+                <div className="name">
+                    <h3>H.E POUL KAGAME</h3>
+                </div>
+                </div>
+                <div className="image">
+                    <img src="https://www.paulkagame.com/wp-content/uploads/2021/04/PaulKagame_Web.jpeg" alt="pk"/>
+                
+                <div className="name">
+                    <h3>H.E POUL KAGAME</h3>
+                </div>
+                </div>
+                <div className="image">
+                    <img src="https://www.paulkagame.com/wp-content/uploads/2021/04/PaulKagame_Web.jpeg" alt="pk"/>
+                
+                <div className="name">
+                    <h3>H.E POUL KAGAME</h3>
+                </div>
+                </div>
+                <div className="image">
+                    <img src="https://www.paulkagame.com/wp-content/uploads/2021/04/PaulKagame_Web.jpeg" alt="pk"/>
+                
+                <div className="name">
+                    <h3>H.E POUL KAGAME</h3>
+                </div>
+                </div>
+                
+            </div>
+            
+        </article>
+    </>
+)
+}
+
+export default Speakers
